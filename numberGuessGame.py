7aba15b0f10dd7a8857c771a9d06ca1279dff3f0
adithyaaneesh <adithyaaneesh29@gmail.com>
2025-09-13 11:48:03 +0530

    number guessing game

---
+++ numberGuessGame.py
@@ -0,0 +1,23 @@
+#4. Build a Number Guessing Game (random number 1–100, user keeps guessing until correct).
+
+import random
+def guessNumber():
+    # guessNum = random.randint(1,100)
+    guessNum = 34
+    inputnum = 0
+    attempts = 0
+    print("WELCOME TO NUMBER GUESSING GAME")
+    print("GUESS A RANDOM NUMBER BETWEEN 1-100") 
+    while inputnum!=guessNum:
+        inputnum = int(input("Enter your guess:"))
+        attempts += 1
+        if inputnum > guessNum:
+            print("Too High , Try Again!!")
+        elif inputnum < guessNum:
+            print("Too Low , Try Again!!")
+        else:
+            print(f"You Guessed Right! The number was {guessNum}.")
+            print(f"Attempts taken: {attempts}")
+guessNumber()
+
+#5. Design a Word Guessing (Hangman) game using a word list.
