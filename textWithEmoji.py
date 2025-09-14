d846ae612f2336b352fe3170182b48c6569eb35d
adithyaaneesh <adithyaaneesh29@gmail.com>
2025-09-14 14:36:23 +0530

    take input & make it case sensitive

--- textWithEmoji.py
+++ textWithEmoji.py
@@ -6,10 +6,15 @@ emoji = {
     "super" : "👌",
     "ok" : "👍"
 }
-
 def textreplace():
     text = "are you happy now? if no, then why are you so sad ? nothig!! so you are super cool ok"
+    text = str(input("Enter the text you want to replace: "))
+    # text = "I am super happy today, but my friend is sad. It's ok, we will still have a super day!"
     for word,emj in emoji.items():
         text = text.replace(word,emj)
+        text = text.replace(word.capitalize(),emj)
+        text = text.replace(word.upper(),emj)
+        text = text.replace(word.lower(),emj)
     return text
-print(textreplace())
+print("Converted text : ",textreplace())
+
